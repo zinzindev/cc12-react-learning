acb5d909740293b64b8dfa7f481478843e48ca77
zinzindev <zinzin30@windowslive.com>
2023-08-18 11:54:46 +0700

    create ui for learn JWT Token-Based Authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,77 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+// localStorage.setItem();
+// localStorage.getItem();
+// localStorage.removeItem();
+// localStorage.clear();
+
 function App() {
-	return <div className='App'>test</div>;
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [isLogged, setIsLogged] = useState(false);
+
+	useEffect(() => {
+		if (localStorage.getItem('token')) {
+			setIsLogged(true);
+		}
+	}, []);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			const res = await axios.post('http://localhost:8007/auth/login', {
+				username,
+				password,
+			});
+			console.log(res.data);
+			setIsLogged(true);
+			localStorage.setItem('token', res.data.token);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleClickshow = async () => {
+		try {
+			const res = await axios.get('http://localhost:8007/todos', {
+				headers: {
+					// Authorization: `Bearer ${localStorage.getItem('token')}`,
+					Authorization: 'Bearer ' + localStorage.getItem('token'),
+				},
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<div className='App'>
+			{!isLogged ? (
+				<form onSubmit={handleSubmit}>
+					<input
+						placeholder='username'
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<input
+						placeholder='passowrd'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<button>Login</button>
+				</form>
+			) : (
+				<>
+					<h1>Welcome back {username}</h1>
+					<button onClick={handleClickshow}>show Todo</button>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
