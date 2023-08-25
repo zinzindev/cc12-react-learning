54efaae2801b808c9d2fcb282ee7384f930eac37
zinzindev <zinzin30@windowslive.com>
2023-08-25 19:49:05 +0700

    rerun useReducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
 
+// ACTION: INCREASE, DECREASE
+function reducerFunction(state, action) {
+	switch (action.type) {
+		case 'INCREASE':
+			return { count: state.count + 1 };
+		case 'DECREASE':
+			return { count: state.count - 1 };
+		default:
+			return state;
+	}
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [state, dispatch] = useReducer(reducerFunction, { count: 0 });
+
+	return (
+		<div className='App'>
+			<h1>{state.count}</h1>
+			<button onClick={() => dispatch({ type: 'INCREASE' })}>+</button>
+			<button onClick={() => dispatch({ type: 'DECREASE' })}>-</button>
+		</div>
+	);
 }
 
 export default App;
